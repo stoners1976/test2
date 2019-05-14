b643573171ffbca58a333903f68e3b40215374dc
stoners1976 <50428479+stoners1976@users.noreply.github.com>
2019-05-14 09:02:03 +0900

    Update locustfile.py

--- locustfile.py
+++ locustfile.py
@@ -1,29 +1,35 @@
-from locust import HttpLocust, TaskSet, task
+from locust import HttpLocust, TaskSet
+
+
+
+def login(l):
+
+    l.client.post("/goodnight/api/v3/users/signin/normal", {"email":"joe.cho@email.com", "pw":"password", "social_id":0})
+
+
+
+def profile(l):
+
+    l.client.get("/goodnight/api/v3/users/profile")
+
+
 
 class UserBehavior(TaskSet):
+
+    tasks = {profile: 1}
+
+
+
     def on_start(self):
-        """ on_start is called when a Locust start before any task is scheduled """
-        self.login()
 
-    def on_stop(self):
-        """ on_stop is called when the TaskSet is stopping """
-        self.logout()
+        login(self)
 
-    def login(self):
-        self.client.post("/login", {"username":"ellen_key", "password":"education"})
 
-    def logout(self):
-        self.client.post("/logout", {"username":"ellen_key", "password":"education"})
-
-    @task(2)
-    def index(self):
-        self.client.get("/")
-
-    @task(1)
-    def profile(self):
-        self.client.get("/profile")
 
 class WebsiteUser(HttpLocust):
+
     task_set = UserBehavior
+
     min_wait = 5000
-    max_wait = 9000
+
+    max_wait = 9000 
